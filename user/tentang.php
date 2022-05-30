6cf708a71e10999d593e0f363d4fee02e92c5ff6
AndiBesseAdya <adyaadias87@gmail.com>
2022-05-30 22:20:03 +0800

    halaman tentang

--- user/tentang.php
+++ user/tentang.php
@@ -8,22 +8,20 @@ require 'templates/header.php';
 <div class="row bg-white border p-2 mt-5">
     <div class="col-md ml-3">
         <h1>Tentang Kami</h1>
-        <p class="lead">Kami menjual berbagai perangkat elektronik tepatnya diperangkat Komputer, Laptop, dan Smartphone
-            Kami mulai membuka toko kami pada tahun 2019 bulan Januari dimana saata itu kami hanya menjual 1 jenis produk yaitu Smartphone
-            dan di pertengahan tahun 2020 kami menambahkan 2 jenis barang yaitu berbagai hardware untuk kebutuhan perakitan PC, dan juga
-            mejual berbagai jenis laptop untk mid level
+        <p class="lead">Kami melakukan penjualan pakaian muslim secara online. 
+            Kami mulai membuka toko kami pada tahun 2022 bulan januari dimana saat ini kami menjual berbagai macam produk pakaian muslim seperti, tunik, blouse, kameja, dll.
         </p>
         <hr>
         <div class="row ml-1">
             <div class="w-100">
                 <h3><i class="fa fa-map-marker"></i>Alamat</h3>
-                <p>Lopawon / Kebobang<br>Wonosari / Malang<br>Jawa Timur<br>Indonesia<br><strong>Bayu Pamungkas</strong></p>
+                <p>Jl. Reka A<br>Makassar - Gowa<br>Sulawesi Selatan<br>Indonesia<br><strong>Beauty Shop</strong></p>
             </div>
             <!-- /.col-sm-4-->
             <div class="">
                 <h3><i class="fa fa-phone"></i> Hubungi Kami</h3>
-                <p class="text-muted">Anda dapat menghubungi kami dinomer yang tertera dibawah ini</p>
-                <p><strong>+6287 812 212 333</strong></p>
+                <p class="text-muted">Anda dapat menghubungi kami kontak yang tertera dibawah ini</p>
+                <p><strong class="fa fa-phone"></strong><strong>+6281 234 567 890</strong></p>
             </div>
         </div>
     </div>
