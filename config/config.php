9bb93d0b2bc303751cd42dec8b763be62cb43909
DeaWahsa <deawahsasaputri@gmail.com>
2022-05-30 21:16:36 +0800

    Initial commit

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+/*----- Kofigurasi Database -----*/
+define('hostname', '127.0.0.1');
+define('username', 'root');
+define('password', '');
+define('database', 'shoping');
+
+/*----- Url -----*/
+define('url', 'http://localhost/e-commerce/shoping/');
+
+/*----- Author -----*/
+define('author', 'Rekayasa Website');
